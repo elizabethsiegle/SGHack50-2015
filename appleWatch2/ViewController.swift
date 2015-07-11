488dc5356cab2f3081eed4b02a4d16e495ee802a
Elizabeth Siegle <lizzie.siegle@gmail.com>
2015-07-11 12:23:57 -0700

    Initial Commit

---
+++ appleWatch2/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  appleWatch2
+//
+//  Created by Elizabeth Siegle on 7/11/15.
+//  Copyright (c) 2015 Elizabeth Siegle. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
