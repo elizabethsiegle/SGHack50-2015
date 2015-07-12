fa2aee625182fe03cec05fc6d7265a382c0a2281
Elizabeth Siegle <lizzie.siegle@gmail.com>
2015-07-11 19:56:49 -0700

    separate InterfaceControllers for each page

---
+++ page2InterfaceController.swift
@@ -0,0 +1,42 @@
+//
+//  page1InterfaceController.swift
+//  appleWatch2
+//
+//  Created by Elizabeth Siegle on 7/11/15.
+//  Copyright (c) 2015 Elizabeth Siegle. All rights reserved.
+//
+
+import WatchKit
+import Foundation
+
+
+class page2InterfaceController: WKInterfaceController {
+    
+    override func awakeWithContext(context: AnyObject?) {
+        super.awakeWithContext(context)
+        
+        // Configure interface objects here.
+    }
+    @IBOutlet weak var page2Label: WKInterfaceLabel!
+    
+    override func willActivate() {
+        // This method is called when watch view controller is about to be visible to user
+        super.willActivate()
+        //initialize array
+        let memory1=["text":"apple"]
+        let memory2=["text":"banana"]
+        
+        var memoryArray:[Dictionary<String, String>]=[]
+        memoryArray+=[memory1]
+        memoryArray+=[memory2]
+        
+        //self.page2Label.setText(memoryArray[1])
+
+    }
+    
+    override func didDeactivate() {
+        // This method is called when watch view controller is no longer visible
+        super.didDeactivate()
+    }
+    
+}
---
+++ page3InterfaceController.swift
@@ -0,0 +1,32 @@
+//
+//  page1InterfaceController.swift
+//  appleWatch2
+//
+//  Created by Elizabeth Siegle on 7/11/15.
+//  Copyright (c) 2015 Elizabeth Siegle. All rights reserved.
+//
+
+import WatchKit
+import Foundation
+
+
+class page3InterfaceController: WKInterfaceController {
+    
+    override func awakeWithContext(context: AnyObject?) {
+        super.awakeWithContext(context)
+        
+        // Configure interface objects here.
+    }
+    @IBOutlet weak var page3Label: WKInterfaceLabel!
+    
+    override func willActivate() {
+        // This method is called when watch view controller is about to be visible to user
+        super.willActivate()
+    }
+    
+    override func didDeactivate() {
+        // This method is called when watch view controller is no longer visible
+        super.didDeactivate()
+    }
+    
+}
